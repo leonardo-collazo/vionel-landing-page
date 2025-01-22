10fc0c9b5a2e5b788d351541122dc8778445065c
Leonardo Collazo Klenina <kidbourbongamer@gmail.com>
2025-01-22 12:24:54 -0500

    Hero section complete.

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,20 @@ module.exports = {
         "mobile-large": "425px",
         "mobile-medium": "375px",
       },
+      backgroundImage: {
+        "hero-background-large-vertical":
+          "url('/src/assets/pictures/computer-hardware-large-vertical.jpg')",
+        "hero-background-medium-vertical":
+          "url('/src/assets/pictures/computer-hardware-medium-vertical.jpg')",
+        "hero-background-small-vertical":
+          "url('/src/assets/pictures/computer-hardware-small-vertical.jpg')",
+        "hero-background-large-horizontal":
+          "url('/src/assets/pictures/computer-hardware-large-horizontal.jpg')",
+        "hero-background-medium-horizontal":
+          "url('/src/assets/pictures/computer-hardware-medium-horizontal.jpg')",
+        "hero-background-small-horizontal":
+          "url('/src/assets/pictures/computer-hardware-small-horizontal.jpg')",
+      },
     },
   },
   plugins: [],
